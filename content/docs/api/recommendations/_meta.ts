6fecdd55082ed28ec4a0f52825fbcdf2ed8d6bfa
0xthrpw <0xthrpw@gmail.com>
2026-04-08 01:54:27 -0400

    docs(recommendations): also-viewed, similar-to-watchlist, based-on-votes, and personalized for-you blend

--- content/docs/api/recommendations/_meta.ts
+++ content/docs/api/recommendations/_meta.ts
@@ -1,3 +1,7 @@
 export default {
   index: 'Overview',
+  'also-viewed': 'Also Viewed',
+  'similar-to-watchlist': 'Similar to Watchlist',
+  'based-on-votes': 'Based on Votes',
+  'for-you': 'For You',
 }
