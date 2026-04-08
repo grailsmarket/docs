de415bd9e2814ae8d317c6748703e12606225714
0xthrpw <0xthrpw@gmail.com>
2026-04-08 00:24:54 -0400

    docs(cart): get with enriched data, summary counts, add/bulk-add, remove, and clear by type

--- content/docs/api/cart/_meta.ts
+++ content/docs/api/cart/_meta.ts
@@ -1,3 +1,9 @@
 export default {
   index: 'Overview',
+  'get-cart': 'Get Cart',
+  summary: 'Cart Summary',
+  add: 'Add to Cart',
+  'bulk-add': 'Bulk Add',
+  remove: 'Remove Item',
+  clear: 'Clear Cart',
 }
